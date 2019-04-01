2c9847d7fae9f25ce4fade6c66c6cc0236fe42e6
Admin <Admin@192.168.124.148>
2019-04-01 11:29:50 +0200

    Separando el BackEnd del FrontEnd

---
+++ backprueba1/src/main/java/BackEnd/backprueba1/SpringPrimeFacesApplication.java
@@ -0,0 +1,15 @@
+package BackEnd.backprueba1;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@SpringBootApplication
+@EnableJpaRepositories
+public class SpringPrimeFacesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringPrimeFacesApplication.class, args);
+	}
+}
